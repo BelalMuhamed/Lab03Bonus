955d77b6d48389f96560e5521d0bc61b93a59cf4
BelalBasal <basdibrahim@gmail.com>
2024-11-15 10:24:04 +0200

    Add project files.

---
+++ Bonus/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Bonus
+{
+    internal class Program
+    {
+      
+
+        #region Bonus
+        public static void Swap(ref TimeSpan T01,ref TimeSpan T02)
+        {
+            TimeSpan T03= new TimeSpan();
+            T03 = T01;
+            T01=T02;
+            T02 = T03;
+        }
+        public static void SortingEdition02(TimeSpan[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j =  1; j < array.Length; j++)
+                {
+                    if (array[i].TotalSeconds() > array[j].TotalSeconds())
+                    {
+                        Swap(ref array[i], ref array[j]);
+                    }
+                }
+            }
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            
+            //take size from user 
+            bool flag = true;
+            int size;
+            do
+            {
+                Console.WriteLine("Enter the size of time span array");
+                flag= int.TryParse(Console.ReadLine(), out size);
+            }
+            while(!flag);
+            //filling array from user 
+            TimeSpan[] ArrayOfTimes=new TimeSpan[size];
+            for(int i = 0;i<ArrayOfTimes.Length;i++)
+            {
+                Console.WriteLine($"Enter hours of the   {i+1} time ");
+                ArrayOfTimes[i].Hours = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Enter minutes of the  {i + 1} time ");
+                ArrayOfTimes[i].Minutes = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Enter seconds of the  {i + 1} time ");
+                ArrayOfTimes[i].Seconds = int.Parse(Console.ReadLine());
+            }
+            // view array
+            Console.WriteLine("Times you enterd ");
+            for(int i = 0;i<ArrayOfTimes.Length;i++)
+            {
+                ArrayOfTimes[i].GetString();
+            }
+            //test the function
+            SortingEdition02(ArrayOfTimes);
+            Console.WriteLine("Array after sorting using second edition ");
+            for (int i = 0; i < ArrayOfTimes.Length; i++)
+            {
+                ArrayOfTimes[i].GetString();
+            }
+        }
+    }
+}
---
+++ Bonus/TimeSpan.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus
+{
+    internal struct TimeSpan
+    {
+        #region hours full property
+        private int hours;
+
+        public int Hours
+        {
+            get { return hours; }
+            set
+            {
+                if (value >= 1 && value <= 12)
+                    hours = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        #endregion
+
+        #region minutes full property
+        private int minutes;
+
+        public int Minutes
+        {
+            get { return minutes; }
+            set
+            {
+                if (value >= 0 && value < 60)
+                    minutes = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        #endregion
+
+        #region seconds full property
+        private int seconds;
+
+        public int Seconds
+        {
+            get { return seconds; }
+            set
+            {
+                if (value >= 0 && value < 60)
+                    seconds = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        #endregion
+
+        
+
+        #region Convert to Seconds
+        public long TotalSeconds()
+        {
+            return (this.hours * 60 * 60) + (this.minutes * 60) + this.seconds;
+        }
+        #endregion
+
+        #region print
+        public void GetString()
+        {
+            Console.WriteLine($"{this.hours} :: {this.minutes} :: {this.seconds}");
+        }
+        #endregion
+    }
+}
